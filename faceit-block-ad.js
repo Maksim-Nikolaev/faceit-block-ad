fb6a3215ed636be041cbb5ebcf9b8ee80a7e6560
Maksim Nikolaev <maksim.nikolaev.dev@gmail.com>
2024-04-19 23:37:33 +0200

    Create faceit-block-ad.js

---
+++ faceit-block-ad.js
@@ -0,0 +1,31 @@
+// ==UserScript==
+// @name         Block Background Image
+// @namespace    https://www.faceit.com/
+// @version      0.1
+// @description  Blocks a specific background image on a website
+// @author       Bronix
+// @match        *://www.faceit.com/*
+// @grant        none
+// @run-at       document-end
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=faceit.com
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Define the CSS selector for the element with the background image
+    const targetSelector = '.bHBSNk';
+
+    // Function to remove the background image
+    function removeBackgroundImage() {
+        const targetElement = document.querySelector(targetSelector);
+        if (targetElement) {
+            targetElement.style.background = 'none';
+        }
+    }
+
+    // Run the script when the page loads
+    window.addEventListener('load', () => {
+        setTimeout(removeBackgroundImage, 3500); // Wait 3500ms before removing the background
+    });
+})();
