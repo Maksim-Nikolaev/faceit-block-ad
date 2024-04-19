dc28319f0d0b77b3b77ad52117e232859d841def
Maksim Nikolaev <maksim.nikolaev.dev@gmail.com>
2024-04-19 23:42:39 +0200

    Update faceit-block-ad.user.js

--- faceit-block-ad.user.js
+++ faceit-block-ad.user.js
@@ -8,6 +8,7 @@
 // @grant        none
 // @run-at       document-end
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=faceit.com
+// @homepageURL  https://github.com/Maksim-Nikolaev/faceit-block-ad
 // ==/UserScript==
 
 (function() {
