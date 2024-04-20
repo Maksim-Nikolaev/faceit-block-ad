0ecb8531bb84f20ac83150582aa48347c90918a6
Maksim Nikolaev <maksim.nikolaev.dev@gmail.com>
2024-04-20 13:46:19 +0200

    Remove ads on "ready" popup

--- faceit-block-ad.user.js
+++ faceit-block-ad.user.js
@@ -15,18 +15,33 @@
     'use strict';
 
     // Define the CSS selector for the element with the background image
-    const targetSelector = '.bHBSNk';
+    const backgroundSelector = '.bHBSNk';
+
+    // Define the CSS selector for the popup element
+    const popupSelector = '.lcQvgf';
 
     // Function to remove the background image
     function removeBackgroundImage() {
-        const targetElement = document.querySelector(targetSelector);
-        if (targetElement) {
-            targetElement.style.background = 'none';
+        const backgroundElement = document.querySelector(backgroundSelector);
+        if (backgroundElement) {
+            backgroundElement.style.background = 'none';
+        }
+    }
+
+    // Function to remove the popup background
+    function removePopupBackground() {
+        const popupElement = document.querySelector(popupSelector);
+        if (popupElement) {
+            popupElement.style.backgroundImage = 'none';
         }
     }
 
     // Run the script when the page loads
     window.addEventListener('load', () => {
         setTimeout(removeBackgroundImage, 3500); // Wait 3500ms before removing the background
+        checkPopup();
     });
+
+    // Check for the popup every 3500ms
+    setInterval(checkPopup, 3500);
 })();
